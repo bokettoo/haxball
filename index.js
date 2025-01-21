d749535d96060a4de1f20deed6b1e59950e85795
Anass <146187264+bokettoo@users.noreply.github.com>
2025-01-21 22:33:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ app.get("/run-script", async (req, res) => {
     res.json({ message: result });
   } catch (error) {
     console.error("Error executing script:", error);
-    res.status(500).json({ message: "An error occurred", error: error.message });
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
   } finally {
     scriptRunning = false;
   }
